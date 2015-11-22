e5ffa82185926b031177b58f107065f53d1c278b
Chi Shen <shenchi710@gmail.com>
2015-11-23 01:48:13 +0800

    add projects with images

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
 $(function () {
     "use strict";
+    
+    // data
+    
+    
+    // scrollspy
     $('body').on('click', '.page-scroll a', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 800, 'easeInOutExpo');
         event.preventDefault();
+    });
+    
+    // image carousel
+    $('.image_slider').slick({
+        infinite: false,
+        speed: 150,
+        draggable: false,
+        variableWidth: true
+    });
+    
+    // image lightbox
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
     });
 });
